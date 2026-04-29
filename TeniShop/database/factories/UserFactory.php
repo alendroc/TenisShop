eee8836177bad5c27dd993d756a4f3d6c1af15c3
Randall_Wa <randall.alvarez.ch@gmail.com>
2026-04-29 15:45:04 -0600

    8th trying to deploy none monoproject bs

--- TeniShop/database/factories/UserFactory.php
+++ TeniShop/database/factories/UserFactory.php
@@ -23,15 +23,15 @@ class UserFactory extends Factory
      * @return array<string, mixed>
      */
     public function definition(): array
-{
-    return [
-        'name' => $this->faker->name(),
-        'email' => $this->faker->unique()->safeEmail(),
-        'email_verified_at' => now(),
-        'password' => static::$password ??= Hash::make('password'),
-        'remember_token' => Str::random(10),
-    ];
-}
+    {
+        return [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ];
+    }
 
     /**
      * Indicate that the model's email address should be unverified.
