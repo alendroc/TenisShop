f162161dba0d1ed0480758fe3b70cdc1136f781c
Randall_Wa <randall.alvarez.ch@gmail.com>
2026-04-29 21:21:15 -0600

    11th trying to deploy none monoproject bs

--- TeniShop/docker/start.sh
+++ TeniShop/docker/start.sh
@@ -8,7 +8,7 @@ APP_NAME="${APP_NAME:-TenisShop}"
 APP_ENV="${APP_ENV:-production}"
 APP_KEY="${APP_KEY:-}"
 APP_DEBUG="${APP_DEBUG:-false}"
-APP_URL="${APP_URL:-http://localhost}"
+APP_URL="${APP_URL:-https://tenisshop.onrender.com}"
 
 LOG_CHANNEL=stderr
 LOG_LEVEL=error
