0bc4aa92f1d4c47e97446df8c4021451dc62e63b
Alejandro <alejandro6chaves@gmail.com>
2026-05-30 09:59:01 -0600

    api funcio nando

--- TeniShop/config/cors.php
+++ TeniShop/config/cors.php
@@ -1,4 +1,21 @@
-'paths' => ['api/*'],
-'allowed_origins' => ['http://localhost:5173'],
-'allowed_methods' => ['*'],
-'allowed_headers' => ['*'],
+<?php
+
+return [
+
+    'paths' => ['api/*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['http://localhost:5173'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
